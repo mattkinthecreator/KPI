fbf206eb29d118ce95b408202e7c520557315af7
Matthew <bekievbeil@gmail.com>
2021-10-22 12:41:00 +0600

    add new modal

--- js/main.js
+++ js/main.js
@@ -6,25 +6,25 @@ let wKPI = $('#weeklyKPI')
 let mKPI = $('#monthlyKPI')
 let contacts = $('.contacts')
 
-$('#btn_add').on('click', function(){
-    let obj = {
-        name: userName.val(),
-        lastName: lastName.val(),
-        phone: number.val(),
-        weeklyKpi: wKPI.val(),
-        monthlyKpi: mKPI.val()
-    }
-    addInfo(obj)
+$('#btn_add').on('click', function () {
+  let obj = {
+    name: userName.val(),
+    lastName: lastName.val(),
+    phone: number.val(),
+    weeklyKpi: wKPI.val(),
+    monthlyKpi: mKPI.val(),
+  }
+  addInfo(obj)
 })
 
-
-function addInfo(obj){  
-    fetch('http://localhost:8000/students' ,{
-        method: "POST", 
-        body: JSON.stringify(obj), 
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
- }}) 
+function addInfo(obj) {
+  fetch('http://localhost:8000/students', {
+    method: 'POST',
+    body: JSON.stringify(obj),
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+  })
 }
 
 $('body').on('click', '.btn-delete', (e) => {
@@ -38,6 +38,11 @@ function deleteStudent(id) {
   })
 }
 
+$('body').on('click', '.btn-edit', (e) => {
+  let id = e.target.parentNode.id
+  editStudent(id)
+})
+
 function editStudent(id) {
   let obj = {
     name: '',
@@ -49,12 +54,12 @@ function editStudent(id) {
 }
 
 async function render() {
-   let res = await fetch(API)
-   let data = await res.json();
+  let res = await fetch(API)
+  let data = await res.json()
 
-    data.forEach(item => {
-        contacts.append(
-        `<div id="${item.id}">
+  data.forEach((item) => {
+    contacts.append(
+      `<div id="${item.id}">
             <img class="btn-edit" src="./img/pen.png">
             <p>${item.name}</p>
             <p>${item.lastName}</p>
@@ -62,9 +67,10 @@ async function render() {
             <p>${item.weeklyKpi}</p>
             <p>${item.monthlyKpi}</p>
             <img class="btn-delete" src="./img/delete.png">
-        </div>`)
-    });
-   console.log(data)
+        </div>`
+    )
+  })
+  console.log(data)
 }
 
 render()
