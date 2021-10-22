7d7651f0184fd0c3ba639ae6ed3b95a3e9e86e81
Maadanov <maadanov777.@gmail.com>
2021-10-22 11:54:54 +0600

    add create student func

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
-const API = `http://localhost:8000/students`
+const API = 'http://localhost:8000/students'
 let userName = $('#name')
 let lastName = $('#lastName')
 let number = $('#phoneNumber')
 let wKPI = $('#weeklyKPI')
+let mKPI = $('#monthlyKPI')
 
-$('.btn-add').on('click', async function(){
+$('#btn_add').on('click', function(){
     let obj = {
-        name: userName.val()
+        name: userName.val(),
+        lastName: lastName.val(),
+        phone: number.val(),
+        weeklyKpi: wKPI.val(),
+        monthlyKpi: mKPI.val()
     }
+    addInfo(obj)
 })
+
+
+function addInfo(obj){  
+    fetch('http://localhost:8000/students' ,{
+        method: "POST", 
+        body: JSON.stringify(obj), 
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+ }}) 
+}
